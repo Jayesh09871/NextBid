1c8493183893979712c2d68ab300ed927afe968c
Jayesh09871 <jayeshkakhani29@gmail.com>
2024-12-16 17:47:15 +0530

    updated

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,8 +6,9 @@ import { AppConfig } from "../config/env.config.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const token = req.cookies.token;
+  console.log("heo" , token)
   if (!token) {
-    return next(new ErrorHandler("User not authenticated.", 400));
+    return next(new ErrorHandler("User not Authenticated.", 400));
   }
   const decoded = jwt.verify(token, AppConfig.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
