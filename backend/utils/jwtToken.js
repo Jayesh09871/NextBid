7f247e9f45e5ff38a7e97373dcbdf070f0df6302
Jayesh09871 <jayeshkakhani29@gmail.com>
2024-12-17 09:24:06 +0530

    updated code

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,8 +9,8 @@ export const generateToken = (user, message, statusCode, res) => {
   .cookie("token", token, {
     maxAge: cookieExpiresIn * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true, // Required if using HTTPS
-    sameSite: "None", // Use "Strict" or "Lax" if the frontend and backend are on the same domain
+    secure:true,
+    sameSite:"None",
   })
   .json({
     success: true,
